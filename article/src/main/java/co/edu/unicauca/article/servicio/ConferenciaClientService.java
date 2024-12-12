5f30e567c0e34bfaeb33f87ea865349e40d4b972
dfelipe7 <duvanfelipe777@outlook.com>
2024-12-12 18:46:19 -0500

    actulizacion para usar docker y modificacacion de controller

---
+++ article/src/main/java/co/edu/unicauca/article/servicio/ConferenciaClientService.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.unicauca.article.servicio;
+
+import co.edu.unicauca.article.dto.ConferenceDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Servicio para la interacción con el microservicio de conferencias.
+ * Proporciona métodos para obtener información sobre conferencias.
+ *
+ * @author Daniel Muñoz
+ * @author Jesus Iles
+ * @author Esteban Martinez
+ * @author Felipe Armero
+ */
+@Service
+public class ConferenciaClientService {
+
+    private final RestTemplate restTemplate;
+    @Value("${conference.service.url}")
+    private String conferenceServiceUrl;
+
+    /**
+     * Constructor de ConferenciaClientService.
+     *
+     * @param restTemplate Instancia de RestTemplate utilizada para realizar las
+     * peticiones HTTP.
+     */
+    public ConferenciaClientService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    /**
+     * Obtiene una conferencia por su ID.
+     *
+     * @param conferenciaId El ID de la conferencia que se va a obtener.
+     * @return Un objeto ConferenceDTO que representa la conferencia.
+     */
+    public ConferenceDTO obtenerConferencia(Long conferenciaId) {
+        String url = conferenceServiceUrl + "/api/conferences/" + conferenciaId;
+        return restTemplate.getForObject(url, ConferenceDTO.class);
+    }
+}
